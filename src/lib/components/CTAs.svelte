8495c4b584f6f5ed03baa62feea0fc02512dcd83
Paul Gapud <campusinvasionproject@gmail.com>
2024-07-29 00:37:44 +0800

    sign in popup dialog

--- src/lib/components/CTAs.svelte
+++ src/lib/components/CTAs.svelte
@@ -2,18 +2,22 @@
 // @ts-nocheck
 
 	import { Button } from "./ui/button";
-    import { darkMode } from '$str';
+	import * as Dialog from "./ui/dialog";
+	import {Input} from "./ui/input";
+	import {Label} from "./ui/label";
 
-    // To use the store in the component
-    let dark;
-    darkMode.subscribe(value => {
-        dark = value;
-    });
+	import { darkMode } from '$str';
 
-    function toggleDarkMode() {
-        darkMode.update(value => !value);
-        console.log("Toggled dark mode");
-    }
+	// To use the store in the component
+	let dark;
+	darkMode.subscribe(value => {
+		dark = value;
+	});
+
+	function toggleDarkMode() {
+		darkMode.update(value => !value);
+		console.log("Toggled dark mode");
+	}
 </script>
 
 <div class="flex items-center gap-4">
@@ -24,10 +28,41 @@
             As Guest</p>
     </button>
 
-    <button class={"btn primary" + (dark ? "-dark" : " ")}>
-        <p class="text-base sm:text-lg md:text-xl">
-            <i class="fa-solid fa-user-tie text-xl">&nbsp;</i>
-            As Admin</p>
-    </button>
+    <Dialog.Root>
+        <Dialog.Trigger>
+
+				<button class={"btn primary" + (dark ? "-dark" : " ")}>
+					<p class="text-base sm:text-lg md:text-xl">
+					<i class="fa-solid fa-user-tie text-xl">&nbsp;</i>
+					As Admin</p>
+				</button>
+
+        </Dialog.Trigger>
+        
+				<Dialog.Content class="p-10">
+					<Dialog.Header>
+						<Dialog.Title>
+							<i class="fa-solid fa-exclamation-triangle text-l">&nbsp;</i>
+							Admin Access Only</Dialog.Title>
+						<Dialog.Description class="pt-4 text-justify text-base">
+							This system is only for authorized users of the Pahinungód. Unauthorized access is prohibited and may result in legal action under the Data Privacy Act of the Philippines.
+						</Dialog.Description>
+					</Dialog.Header>
+					<div class="grid gap-4 py-3">
+						<div class="grid grid-cols-4 items-center gap-4">
+							<Label for="name" class="text-right">Username</Label>
+							<Input id="name" value="paul@pahi" class="col-span-3" />
+						</div>
+						<div class="grid grid-cols-4 items-center gap-4">
+							<Label for="username" class="text-right">Password</Label>
+							<Input id="username" value="pass" class="col-span-3" />
+						</div>
+					</div>
+					<Dialog.Footer>
+						<Button type="submit" class="text-sm">Sign In</Button>
+					</Dialog.Footer>
+				</Dialog.Content>
+
+      </Dialog.Root>
 
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,23 +6,11 @@
   const logoURL = new URL('../assets/pahi-logo.png', import.meta.url).href;
 </script>
 
-
-<!-- <img src="{logoURL}" class="max-w-xs" alt="" />
-<h1 class="text-yellow-400 text-xl font-extrabold">PahiBaguio</h1>
-<p>Ugnayan ng Pahinungód</p>
-<p>Volunteer Database</p> -->
-
-<SectionWrapper id="landing">
-  <Header/>
-  <Hero/>
-</SectionWrapper>
+<main>
+  <SectionWrapper id="landing">
+    <Header/>
+    <Hero/>
+  </SectionWrapper>
 
  <About/>
-
-<nav>
-  <a href="/">Home</a>
-  <a href="/db">database</a>
-  <!-- <a href="/contact">Contact</a>
-  <a href="/user/1">User 1 Profile</a>
-  <a href="/user/2">User 2 Profile</a> -->
-</nav>
+</main>

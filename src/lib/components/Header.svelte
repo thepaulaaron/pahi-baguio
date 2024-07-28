c91a1ce1f7514c6e511df736a085de958df44e07
Paul Gapud <campusinvasionproject@gmail.com>
2024-07-28 23:19:41 +0800

    dark/light mode landing page

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,6 @@
 <script>
     import { openModal } from "../../store";
     import DarkToggle from "./dark-toggle.svelte";
-	import Button from "./ui/button/button.svelte";
 </script>
 
 <header class="flex flex-col relative z-20">
@@ -9,8 +8,8 @@
         class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6"
     >
         <a href="/">
-            <h1 class="font-semibold">
-                Swoley <span class="text-indigo-400">Moley</span>
+            <h1 class="font-medium">
+               UP | Baguio
             </h1>
         </a>
         <button

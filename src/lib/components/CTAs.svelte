39a17732f62de25f985572424b6ac767357f2395
Paul Gapud <campusinvasionproject@gmail.com>
2024-07-28 22:34:36 +0800

    cta light mode

--- src/lib/components/CTAs.svelte
+++ src/lib/components/CTAs.svelte
@@ -16,20 +16,12 @@
     }
 </script>
 
-<!-- <button on:click={toggleDarkMode}>adfdf</button> -->
-
 <div class="flex items-center gap-4">
 
-    <button on:click={toggleDarkMode} class={"button specialBtn " + (dark ? " bg-[#181b34]" : " ")}>
+    <button class={"btn secondary" + (dark ? "-dark" : " ")}>
         <p class="text-base sm:text-lg md:text-xl">
             <i class="fa-solid fa-user text-xl">&nbsp;</i>
             As Guest</p>
-    </button>
-
-    <button class={"button specialBtnDark" + (dark ? " bg-[#d58a00]" : " ")}>
-        <p class="text-base sm:text-lg md:text-xl">
-            <i class="fa-solid fa-user-tie text-xl">&nbsp;</i>
-            As Admsin</p>
     </button>
 
     <button class={"btn primary" + (dark ? "-dark" : " ")}>

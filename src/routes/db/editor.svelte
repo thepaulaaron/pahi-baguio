519734878e70581776451c0c9897d56de5c33d67
Paul Gapud <campusinvasionproject@gmail.com>
2024-09-09 00:28:51 +0800

    editor better input field - name

--- src/routes/db/editor.svelte
+++ src/routes/db/editor.svelte
@@ -5,6 +5,7 @@
   import { updateData as updator } from './sort';
   import { selectedVolunteerId } from '$lib/context'; // Import the selected volunteer ID store
   import { activeTab } from "$str"; // Import active tab store
+	import { Separator } from "$comp/ui/separator";
 
   // Prop received from parent
   export let data: Volunteer[];
@@ -92,32 +93,76 @@
 
 {#if selectedVolunteer}
   <div class="volunteer">
-    <!-- Bind volunteer data to input fields -->
-    <input bind:value={selectedVolunteer.Fname} placeholder="First Name:" />
-    <input bind:value={selectedVolunteer.Midname} placeholder="Middle Name:" />
-    <input bind:value={selectedVolunteer.Surname} placeholder="Surname:" />
-    <input bind:value={selectedVolunteer.Suffixname} placeholder="Suffix:" />
-    <input bind:value={selectedVolunteer.Birthday} placeholder="Birthday:" />
-    <input bind:value={selectedVolunteer.MobileNum} placeholder="Mobile Num:" />
-    <input bind:value={selectedVolunteer.PersonalMail} placeholder="Personal Mail:" />
-    <input bind:value={selectedVolunteer.Address} placeholder="Address:" />
-    <input bind:value={selectedVolunteer.UPMail} placeholder="UP Mail:" />
-    <input bind:value={selectedVolunteer.StudentNumber} placeholder="Student Number:" />
-    <input bind:value={selectedVolunteer.DegreeProgram} placeholder="Degree Program:" />
-    <input bind:value={selectedVolunteer.College} placeholder="College:" />
-    <input bind:value={selectedVolunteer.Department} placeholder="Department:" />
-    <input bind:value={selectedVolunteer.Designation} placeholder="Designation:" />
-    <input bind:value={selectedVolunteer.EmergencyContactName} placeholder="Emergency Contact Name:" />
-    <input bind:value={selectedVolunteer.EmergencyContactRelationship} placeholder="Relationship:" />
-    <input bind:value={selectedVolunteer.EmergencyContactNumber} placeholder="Contact Number:" />
-    <input bind:value={selectedVolunteer.VolunteerStatus} placeholder="Volunteer Status:" />
-    <input bind:value={selectedVolunteer.VolunteerSince} placeholder="Volunteer Since:" />
-    <input bind:value={selectedVolunteer.DatabaseID} placeholder="Database ID:" />
-    <input bind:value={selectedVolunteer.Notes} placeholder="Notes:" />
-    <!-- Trigger the save operation -->
-    <button on:click={() => openDialog(data.findIndex(volunteer => volunteer._id === selectedVolunteer._id))}>
-      Save
-    </button>
+
+    <div class="space-y-6">
+      <div>
+        <div class="text-lg font-medium">Personal Information</div>
+        <p class="text-muted-foreground text-sm">Details of {selectedVolunteer.Name}</p>
+        <Separator />
+      </div>
+
+      <!-- <div id="empty"></div> -->
+
+      <div id="name"
+        class="flex space-x-4 pahi-input">
+        <div class="w-[30%]">
+          <p class="text-muted-foreground text-sm">First Name</p>
+          <input bind:value={selectedVolunteer.Fname} placeholder="First Name"/>
+        </div>
+
+        <div class="w-[30%]">
+          <p class="text-muted-foreground text-sm">Middle Name</p>
+          <input bind:value={selectedVolunteer.Midname} placeholder="Middle Name"/>
+        </div>
+
+        <div class="w-[30%]">
+          <p class="text-muted-foreground text-sm">Last Name</p>
+          <input bind:value={selectedVolunteer.Surname} placeholder="Last Name"/>
+        </div>
+        
+        <div class="w-[10%]">
+          <p class="text-muted-foreground text-sm">Suffix</p>
+          <input bind:value={selectedVolunteer.Suffixname} placeholder="Suffix"/>
+        </div>
+      </div>
+
+      <div id="birthday"
+        class="flex space-x-4 pahi-input">
+        <div>
+          <p class="text-muted-foreground text-sm">Birthday</p>
+          <input bind:value={selectedVolunteer.Birthday} placeholder="Birthday:" />
+        </div>
+      </div>
+    </div>
+  <div>
+
+
+
+      
+      
+      <input bind:value={selectedVolunteer.MobileNum} placeholder="Mobile Num:" />
+      <input bind:value={selectedVolunteer.PersonalMail} placeholder="Personal Mail:" />
+      <input bind:value={selectedVolunteer.Address} placeholder="Address:" />
+      <input bind:value={selectedVolunteer.UPMail} placeholder="UP Mail:" />
+      <input bind:value={selectedVolunteer.StudentNumber} placeholder="Student Number:" />
+      <input bind:value={selectedVolunteer.DegreeProgram} placeholder="Degree Program:" />
+      <input bind:value={selectedVolunteer.College} placeholder="College:" />
+      <input bind:value={selectedVolunteer.Department} placeholder="Department:" />
+      <input bind:value={selectedVolunteer.Designation} placeholder="Designation:" />
+      <input bind:value={selectedVolunteer.EmergencyContactName} placeholder="Emergency Contact Name:" />
+      <input bind:value={selectedVolunteer.EmergencyContactRelationship} placeholder="Relationship:" />
+      <input bind:value={selectedVolunteer.EmergencyContactNumber} placeholder="Contact Number:" />
+      <input bind:value={selectedVolunteer.VolunteerStatus} placeholder="Volunteer Status:" />
+      <input bind:value={selectedVolunteer.VolunteerSince} placeholder="Volunteer Since:" />
+      <input bind:value={selectedVolunteer.DatabaseID} placeholder="Database ID:" />
+      <input bind:value={selectedVolunteer.Notes} placeholder="Notes:" />
+      <!-- Trigger the save operation -->
+      <button on:click={() => openDialog(data.findIndex(volunteer => volunteer._id === selectedVolunteer._id))}>
+        Save
+      </button>
+    </div>
+
+
   </div>
 {:else}
   <p>No volunteer selected</p>

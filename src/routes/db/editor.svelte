fe1852cf434a0f410b8046e4b8d6b297edda4d63
Paul Gapud <campusinvasionproject@gmail.com>
2024-10-10 12:48:33 +0800

    reenable save function

--- src/routes/db/editor.svelte
+++ src/routes/db/editor.svelte
@@ -100,17 +100,14 @@
           <div class="text-lg font-medium">Edit Volunteer</div>
           <div>
             <!-- Trigger the save operation -->
-            <!-- <button on:click={() => openDialog(data.findIndex(volunteer => volunteer._id === selectedVolunteer._id))}> -->
-            <button>
-              Save
-            </button>
+            <button on:click={() => openDialog(data.findIndex(volunteer => volunteer._id === selectedVolunteer._id))}>
+                  Save
+              </button>
           </div>
         </div>
-        <!-- <p class="text-muted-foreground text-sm">Details of {selectedVolunteer.Name}</p> -->
+        <p class="text-muted-foreground text-sm">Details of {selectedVolunteer.Name}</p>
         <Separator />
       </div>
-
-      <!-- <div id="empty"></div> -->
 
       <div id="name"
         class="flex space-x-4 pahi-input">
@@ -195,7 +192,6 @@
         <div id="program"
           class="input-ten">
           <p class="text-muted-foreground text-sm">Degree Program</p>
-          <!-- <input bind:value={selectedVolunteer.Birthday} placeholder="Birthday:" /> -->
           <input bind:value={selectedVolunteer.DegProg} placeholder="BS Computer Science" />
         </div>
 
@@ -260,33 +256,12 @@
       </div>
     </div>
   <div>
-
-
-
-      
-      
-      <!-- <input bind:value={selectedVolunteer.MobileNum} placeholder="Mobile Num:" />
-      <input bind:value={selectedVolunteer.PersonalMail} placeholder="Personal Mail:" />
-      <input bind:value={selectedVolunteer.Address} placeholder="Address:" />
-      <input bind:value={selectedVolunteer.UPMail} placeholder="UP Mail:" />
-      <input bind:value={selectedVolunteer.StudentNumber} placeholder="Student Number:" />
-      <input bind:value={selectedVolunteer.DegreeProgram} placeholder="Degree Program:" />
-      <input bind:value={selectedVolunteer.College} placeholder="College:" />
-      <input bind:value={selectedVolunteer.Department} placeholder="Department:" />
-      <input bind:value={selectedVolunteer.Designation} placeholder="Designation:" />
-      <input bind:value={selectedVolunteer.EmergencyContactName} placeholder="Emergency Contact Name:" />
-      <input bind:value={selectedVolunteer.EmergencyContactRelationship} placeholder="Relationship:" />
-      <input bind:value={selectedVolunteer.EmergencyContactNumber} placeholder="Contact Number:" />
-      <input bind:value={selectedVolunteer.VolunteerStatus} placeholder="Volunteer Status:" />
-      <input bind:value={selectedVolunteer.VolunteerSince} placeholder="Volunteer Since:" />
-      <input bind:value={selectedVolunteer.DatabaseID} placeholder="Database ID:" />
-      <input bind:value={selectedVolunteer.Notes} placeholder="Notes:" /> -->
     </div>
 
 
   </div>
-<!-- {:else} -->
-  <!-- <p>No volunteer selected</p> -->
+{:else} 
+<p>No volunteer selected</p>
 {/if}
 
 <!-- {#if $isOpen} -->

ab14382f3a8e20c25e9d7974ba64e65887288067
Paul Gapud <campusinvasionproject@gmail.com>
2024-10-15 11:03:45 +0800

    change mongoURI

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,17 +1,17 @@
 // src/db/mongo.ts
 import { MongoClient, Collection } from 'mongodb';
 
-const mongoURI = 'mongodb+srv://paulgapud:lprXgZ0ZjVEqRxKF@pahibaguio.sazta.mongodb.net/pahi?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://paulgapud:oc9I69pj0WCljX59@moversveltecluster.80qss.mongodb.net/?retryWrites=true&w=majority&appName=moversveltecluster';
 const client = new MongoClient(mongoURI);
-const dbName = 'pahi';
-const collName = 'volunteers';
+const dbName = 'moversveltedatabase';
+const collName = 'moversveltecollection';
 
 // Function to create and return the volunteers collection
 export async function createMongoClient(): Promise<Collection> {
     // Connect to the client if not already connected
     try {
         await client.connect();
-        console.log('MongoDB connected');
+        console.log('MongoDB connected: ' + dbName);
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
         throw error; // Re-throw to handle it in the caller
